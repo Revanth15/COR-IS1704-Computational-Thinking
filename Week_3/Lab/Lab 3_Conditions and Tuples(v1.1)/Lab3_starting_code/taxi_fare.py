38edf087e26a7b5ee039a65b44bb1ca34370ec17
revanth <revanth1509@gmail.com>
2026-09-07 18:35:31 +0800

    update q6 lab 3

--- Week_3/Lab/Lab 3_Conditions and Tuples(v1.1)/Lab3_starting_code/taxi_fare.py
+++ Week_3/Lab/Lab 3_Conditions and Tuples(v1.1)/Lab3_starting_code/taxi_fare.py
@@ -3,7 +3,7 @@ import math
 def calculate_meter_fare(flag_down_fare, rate_400m, rate_350m,
                          distance_travelled):
     total_fare = flag_down_fare
-    if distance_travelled < 1000:
+    if distance_travelled <= 1000:
         pass
     elif distance_travelled < 9800:
         total_fare += math.ceil((distance_travelled - 1000) / 400) * rate_400m
@@ -37,6 +37,7 @@ location_surcharge_cost = 0
 # between_12and6 = True
 # location_surcharge_bool = True
 # location_surcharge_cost = 3.00
+
 flag_down_fare = float(input("What's the flag-downfare? $"))
 rate_400m = float(input("What's the rate per 400 meters within 9.8km? $"))
 rate_350m = float(input("What's the rate per 350 meters beyond 9.8km? $"))
@@ -53,3 +54,41 @@ if location_surcharge_bool:
 total_fare = calculate_total_fare(calculate_meter_fare(flag_down_fare,rate_400m,rate_350m,distance_travelled), calculate_surcharge_modifer(peak_period, between_12and6), location_surcharge_cost) 
 
 print(f"The total fare is ${total_fare}")
+
+
+
+##################
+# Implementation without functions which i initially did. Thought this was cleaner but yes, functions are better.
+
+# flag_down_fare = float(input("What's the flag-downfare? $"))
+# rate_400m = float(input("What's the rate per 400 meters within 9.8km? $"))
+# rate_350m = float(input("What's the rate per 350 meters beyond 9.8km? $"))
+# distance_travelled = int(input("What's the distance travelled (in meters)? "))
+# peak_period = input("Is this ride during a peak period? [yes/no] ") == "yes"
+
+# # Consts
+# location_surcharge_cost = 0
+
+# total_fare = flag_down_fare
+# if distance_travelled <= 1000:
+#     pass
+# elif distance_travelled < 9800:
+#     total_fare += math.ceil((distance_travelled - 1000) / 400) * rate_400m
+# else:
+#     total_fare += (22 * rate_400m) + (math.ceil((distance_travelled - 9800) / 350) * rate_350m)
+
+# surcharge_modifier = 1
+# if peak_period:
+#     surcharge_modifier = 1.25
+# else:
+#     between_12and6 = input("Is this ride between midnight and 6am? [yes/no] ") == "yes"
+#     if between_12and6:
+#         surcharge_modifier = 1.5
+
+# location_surcharge_bool = input("Is there any location surcharge? [yes/no] ") == "yes"
+# if location_surcharge_bool:
+#     location_surcharge_cost = float(input("What's the amount of location surcharge? $"))
+
+# total_fare = round((total_fare * surcharge_modifier) + location_surcharge_cost, 2)
+
+# print(f"The total fare is ${total_fare}")
